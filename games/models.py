252f19c7caab2dc96567aba434bab06f37c0fcf5
Rebecca Conley <rebecca.leigh.conley@gmail.com>
2023-11-09 10:34:15 -0500

    add collection

--- games/models.py
+++ games/models.py
@@ -6,6 +6,14 @@ from django.db import models
 class Game(models.Model):
     name = models.CharField(max_length=255)
     number_of_players = models.IntegerField(blank=True, null=True)
+    collection = models.ForeignKey('Collection', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Collection(models.Model):
+    name = models.CharField(max_length=255)
 
     def __str__(self):
         return self.name
